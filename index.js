ceacbda45799585ec77af0963a1f0d3060dc1294
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-16 11:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ else if (cart.length === 1){
 }
 
 else if (cart.length > 1){
-  for (var j = 0; j < cart.length j++){
+  for (var j = 0; j < cart.length; j++){
   var keys = Object.keys(cart[j]);
   for (var i = 0; i < keys.length; i++){
     var strArr = [];
