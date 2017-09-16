b5ab3b769b40d2671b0281973fbd5075a83ecaee
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-16 11:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ else if (cart.length === 1){
 
 else if (cart.length > 1){
   var keys = Object.keys(cart[0]);
-  for (var i = 0; i < keys.length; i++){
-    var strArr = [];
-    strArr.push(`In your cart, you have ${keys[i]} at $${cart[keys][i]} and`)
-  }
-  
+  // for (var i = 0; i < keys.length; i++){
+  //   var strArr = [];
+  //   strArr.push(`In your cart, you have ${keys[i]} at $${cart[keys][i]} and`)
+  // }
+
 return strArr;
 }
 }
